aedc85a0613442d1a84df1d761d64b4a68be21ea
Nimrat Khalsa <nimratkhalsa16@icloud.com>
2023-09-05 11:07:16 +0530

    Initial Commit

---
+++ InternshipDemo/InternshipDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  InternshipDemoApp.swift
+//  InternshipDemo
+//
+//  Created by Nimrat Khalsa on 05/09/23.
+//
+
+import SwiftUI
+
+@main
+struct InternshipDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
